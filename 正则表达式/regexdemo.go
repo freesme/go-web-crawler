dd1bb202c518574b472cccda3ffe32305803497c
lm <freesme@163.com>
2019-12-10 18:07:52 +0800

    正则

---
+++ 正则表达式/regexdemo.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"regexp"
+	"strings"
+)
+
+func IsIp(ip string) (b bool) {
+	if m, _ := regexp.MatchString("^[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}$", ip); !m {
+		return false
+	}
+	return true
+}
+func IsNum(num string) (b bool) {
+	if m, _ := regexp.MatchString("^[0-9]+$", num); m {
+		return true
+	} else {
+		return false
+	}
+}
+
+func main() {
+	fmt.Println(IsIp("192.168.101.1"))
+	fmt.Println(IsNum("524"))
+
+	resp, err := http.Get("http://www.baidu.com")
+	if err != nil {
+		fmt.Println("http get error.")
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println("http read error")
+		return
+	}
+	src := string(body)
+
+	fmt.Println(src)
+	fmt.Println("--------------------------------------------------------")
+	//将HTML标签全转换成小写
+	re, _ := regexp.Compile("\\<[\\S\\s]+?\\>")
+	src = re.ReplaceAllStringFunc(src, strings.ToLower)
+	//去除STYLE
+	re, _ = regexp.Compile("\\<style[\\S\\s]+?\\</style\\>")
+	src = re.ReplaceAllString(src, "")
+	//去除SCRIPT
+	re, _ = regexp.Compile("\\<script[\\S\\s]+?\\</script\\>")
+	src = re.ReplaceAllString(src, "")
+	//去除所有尖括号内的HTML代码，并换成换行符
+	re, _ = regexp.Compile("\\<[\\S\\s]+?\\>")
+	src = re.ReplaceAllString(src, "\n")
+	//去除连续的换行符
+	re, _ = regexp.Compile("\\s{2,}")
+	src = re.ReplaceAllString(src, "\n")
+	fmt.Println(strings.TrimSpace(src))
+}
